08efb2053d0326944e48683b770e8b8b6ce0f704
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-11-05 17:37:52 +0600

    created booking api for user

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+ `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS_KEY}@cluster0.c3eejtp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const serviceCollection = client.db('ChildCoDB').collection('services');
+    const bookingCollection = client.db('ChildCoDB').collection('booking');
+
+    
+
+
+    // get services
+
+    app.get('/services', async(req,res)=>{
+        try{
+          const cursor = serviceCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+        }
+        catch(error) {
+           console.log(error.message)
+        }
+    })
+
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
+        res.send(result)
+    })
+
+
+    // Booking
+
+    app.post('/booking', async(req, res)=>{
+      const book = req.body;
+
+      // console.log(book)
+
+      // const find = await bookingCollection.findOne(book);
+
+      // console.log(find)
+      // if (!find) {
+      //    const result = await bookingCollection.insertOne(book);
+      //    res.send(result);
+      // }
+
+      // else {
+      //     res.send('duplicate')
+      // }
+
+      // console.log(result);
+
+         const result = await bookingCollection.insertOne(book);
+         res.send(result);
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req,res)=>{
+    res.send('child co server is running');
+})
+
+
+app.listen(port, ()=>{
+    console.log(`child co server is running on port:${port}`)
+})
