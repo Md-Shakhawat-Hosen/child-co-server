408a0a0203150160328225a14720e6792ab2e324
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-11-06 19:25:25 +0600

    created put api for update services

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-require('dotenv').config();
+require("dotenv").config();
 
 //middleware
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
- `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS_KEY}@cluster0.c3eejtp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS_KEY}@cluster0.c3eejtp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,36 +27,88 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db('ChildCoDB').collection('services');
-    const bookingCollection = client.db('ChildCoDB').collection('booking');
+    const serviceCollection = client.db("ChildCoDB").collection("services");
+    const bookingCollection = client.db("ChildCoDB").collection("booking");
 
-    
+    //post services
 
+    app.post("/services", async (req, res) => {
+      try {
+        const service = req.body;
+        const result = await serviceCollection.insertOne(service);
+        res.send(result);
+      } catch (error) {}
+    });
 
-    // get services
+    app.put("/services/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const updateService = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const option = { upsert: true };
+         const addService = {
+           serviceImage,
+           serviceName,
+           serviceDescription,
 
-    app.get('/services', async(req,res)=>{
-        try{
+           serviceProvider,
+           servicePrice,
+           serviceArea,
+         } = updateService;
+
+        const updatedService = {
+          $set:{
+            ...addService
+          }
+        }
+        
+        const result = await serviceCollection.updateOne(filter,updatedService,option)
+
+        // console.log(result)
+        res.send(result)
+
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+
+    app.get("/services", async (req, res) => {
+      try {
+        // const cursor = serviceCollection.find();
+        // const result = await cursor.toArray();
+        // res.send(result);
+
+        //  console.log(req.query);
+
+        let query = {};
+        if (req.query?.email) {
+          query = { "serviceProvider.email": req.query.email };
+          const result = await serviceCollection.find(query).toArray();
+          res.send(result);
+        } else {
           const cursor = serviceCollection.find();
           const result = await cursor.toArray();
           res.send(result);
         }
-        catch(error) {
-           console.log(error.message)
-        }
-    })
 
-    app.get('/services/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await serviceCollection.findOne(query);
-        res.send(result)
-    })
+        // console.log(result)
 
+        //  res.send(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
     // Booking
 
-    app.post('/booking', async(req, res)=>{
+    app.post("/booking", async (req, res) => {
       const book = req.body;
 
       // console.log(book)
@@ -81,14 +127,9 @@ async function run() {
 
       // console.log(result);
 
-         const result = await bookingCollection.insertOne(book);
-         res.send(result);
-    })
-
-
-
-
-
+      const result = await bookingCollection.insertOne(book);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -102,13 +143,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("child co server is running");
+});
 
-
-app.get('/', (req,res)=>{
-    res.send('child co server is running');
-})
-
-
-app.listen(port, ()=>{
-    console.log(`child co server is running on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`child co server is running on port:${port}`);
+});
