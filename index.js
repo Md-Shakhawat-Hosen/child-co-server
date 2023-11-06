55199fb8c0dd9416a5dc0de757616a945db54960
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-11-06 20:07:16 +0600

    created delete api, user can delete their added service

--- index.js
+++ index.js
@@ -72,6 +72,16 @@ async function run() {
       }
     });
 
+
+    app.delete('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const result = await serviceCollection.deleteOne(query);
+      console.log(result)
+      res.send(result);
+    })
+
     app.get("/services", async (req, res) => {
       try {
         // const cursor = serviceCollection.find();
